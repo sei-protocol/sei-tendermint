846e8b9e6e4bfb29d65d51100d4c3a39c3374d72
Brandon Weng <18161326+BrandonWeng@users.noreply.github.com>
2022-12-13 23:08:01 -0500

    revert panic

--- internal/p2p/pex/reactor.go
+++ internal/p2p/pex/reactor.go
@@ -308,7 +308,8 @@ func (r *Reactor) sendRequestForPeers(ctx context.Context, pexCh *p2p.Channel) e
 	defer r.mtx.Unlock()
 	if len(r.availablePeers) == 0 {
 		// no peers are available
-		panic("no available peers to send a PEX request to")
+		r.logger.Error("no available peers to send a PEX request to (retrying)")
+		return nil
 	}
 
 	// Select an arbitrary peer from the available set.
