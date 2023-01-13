71f195a54d14f30146a39fe7f316c6be3c974524
Brandon Weng <18161326+BrandonWeng@users.noreply.github.com>
2023-01-13 15:29:33 -0500

    move around state checks

--- internal/consensus/state.go
+++ internal/consensus/state.go
@@ -1036,10 +1036,13 @@ func (cs *State) handleMsg(ctx context.Context, mi msgInfo, fsyncUponCompletion 
 		// once proposal is set, we can receive block parts
 		err = cs.setProposal(msg.Proposal, mi.ReceiveTime)
 		// See if we can try creating the proposal block if keys exist
-		if cs.config.GossipTransactionKeyOnly && !cs.isProposer(cs.privValidatorPubKey.Address()) && cs.ProposalBlock == nil {
-			created := cs.tryCreateProposalBlock(spanCtx, msg.Proposal.Height, msg.Proposal.Round, msg.Proposal.Header, msg.Proposal.LastCommit, msg.Proposal.Evidence, msg.Proposal.ProposerAddress)
-			if created {
-				cs.fsyncAndCompleteProposal(ctx, fsyncUponCompletion, msg.Proposal.Height, span)
+		if cs.config.GossipTransactionKeyOnly {
+			isProposer := cs.isProposer(cs.privValidatorPubKey.Address())
+			if !isProposer && cs.ProposalBlock == nil {
+				created := cs.tryCreateProposalBlock(spanCtx, msg.Proposal.Height, msg.Proposal.Round, msg.Proposal.Header, msg.Proposal.LastCommit, msg.Proposal.Evidence, msg.Proposal.ProposerAddress)
+				if created {
+					cs.fsyncAndCompleteProposal(ctx, fsyncUponCompletion, msg.Proposal.Height, span)
+				}
 			}
 		}
 
