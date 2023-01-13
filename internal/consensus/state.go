8f8fedf14485522e6b2b5909a70831c301491d91
Brandon Weng <18161326+BrandonWeng@users.noreply.github.com>
2023-01-13 15:31:56 -0500

    sd

--- internal/consensus/state.go
+++ internal/consensus/state.go
@@ -1035,8 +1035,14 @@ func (cs *State) handleMsg(ctx context.Context, mi msgInfo, fsyncUponCompletion 
 		// will not cause transition.
 		// once proposal is set, we can receive block parts
 		err = cs.setProposal(msg.Proposal, mi.ReceiveTime)
+
+		// See if it's possible to be nil
+		if cs.privValidatorPubKey == nil {
+			cs.logger.Info("cs.privValidatorPubKey is nil")
+		}
+
 		// See if we can try creating the proposal block if keys exist
-		if cs.config.GossipTransactionKeyOnly {
+		if cs.config.GossipTransactionKeyOnly && cs.privValidatorPubKey != nil {
 			isProposer := cs.isProposer(cs.privValidatorPubKey.Address())
 			if !isProposer && cs.ProposalBlock == nil {
 				created := cs.tryCreateProposalBlock(spanCtx, msg.Proposal.Height, msg.Proposal.Round, msg.Proposal.Header, msg.Proposal.LastCommit, msg.Proposal.Evidence, msg.Proposal.ProposerAddress)
