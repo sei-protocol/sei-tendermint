3baba84d8958f6aecb90e7c04284c61eec886d64
Philip Su <philip.su.522@gmail.com>
2023-03-08 10:43:04 -0800

    Add mutex to abci client

--- internal/proxy/client.go
+++ internal/proxy/client.go
@@ -2,6 +2,7 @@ package proxy
 
 import (
 	"context"
+	tmsync "github.com/tendermint/tendermint/libs/sync"
 	"io"
 	"os"
 	"syscall"
@@ -59,6 +60,7 @@ type proxyClient struct {
 	logger log.Logger
 
 	client  abciclient.Client
+	mtx     *tmsync.Mutex
 	metrics *Metrics
 }
 
@@ -69,6 +71,7 @@ func New(client abciclient.Client, logger log.Logger, metrics *Metrics) abciclie
 		metrics: metrics,
 		client:  client,
 	}
+	conn.mtx = new(tmsync.Mutex)
 	conn.BaseService = *service.NewBaseService(logger, "proxyClient", conn)
 	return conn
 }
@@ -125,81 +128,113 @@ func kill() error {
 }
 
 func (app *proxyClient) InitChain(ctx context.Context, req *types.RequestInitChain) (*types.ResponseInitChain, error) {
+	app.mtx.Lock()
+	defer app.mtx.Unlock()
 	defer addTimeSample(app.metrics.MethodTiming.With("method", "init_chain", "type", "sync"))()
 	return app.client.InitChain(ctx, req)
 }
 
 func (app *proxyClient) PrepareProposal(ctx context.Context, req *types.RequestPrepareProposal) (*types.ResponsePrepareProposal, error) {
+	app.mtx.Lock()
+	defer app.mtx.Unlock()
 	defer addTimeSample(app.metrics.MethodTiming.With("method", "prepare_proposal", "type", "sync"))()
 	return app.client.PrepareProposal(ctx, req)
 }
 
 func (app *proxyClient) ProcessProposal(ctx context.Context, req *types.RequestProcessProposal) (*types.ResponseProcessProposal, error) {
+	app.mtx.Lock()
+	defer app.mtx.Unlock()
 	defer addTimeSample(app.metrics.MethodTiming.With("method", "process_proposal", "type", "sync"))()
 	return app.client.ProcessProposal(ctx, req)
 }
 
 func (app *proxyClient) ExtendVote(ctx context.Context, req *types.RequestExtendVote) (*types.ResponseExtendVote, error) {
+	app.mtx.Lock()
+	defer app.mtx.Unlock()
 	defer addTimeSample(app.metrics.MethodTiming.With("method", "extend_vote", "type", "sync"))()
 	return app.client.ExtendVote(ctx, req)
 }
 
 func (app *proxyClient) VerifyVoteExtension(ctx context.Context, req *types.RequestVerifyVoteExtension) (*types.ResponseVerifyVoteExtension, error) {
+	app.mtx.Lock()
+	defer app.mtx.Unlock()
 	defer addTimeSample(app.metrics.MethodTiming.With("method", "verify_vote_extension", "type", "sync"))()
 	return app.client.VerifyVoteExtension(ctx, req)
 }
 
 func (app *proxyClient) FinalizeBlock(ctx context.Context, req *types.RequestFinalizeBlock) (*types.ResponseFinalizeBlock, error) {
+	app.mtx.Lock()
+	defer app.mtx.Unlock()
 	defer addTimeSample(app.metrics.MethodTiming.With("method", "finalize_block", "type", "sync"))()
 	return app.client.FinalizeBlock(ctx, req)
 }
 
 func (app *proxyClient) Commit(ctx context.Context) (*types.ResponseCommit, error) {
+	app.mtx.Lock()
+	defer app.mtx.Unlock()
 	defer addTimeSample(app.metrics.MethodTiming.With("method", "commit", "type", "sync"))()
 	return app.client.Commit(ctx)
 }
 
 func (app *proxyClient) Flush(ctx context.Context) error {
+	app.mtx.Lock()
+	defer app.mtx.Unlock()
 	defer addTimeSample(app.metrics.MethodTiming.With("method", "flush", "type", "sync"))()
 	return app.client.Flush(ctx)
 }
 
 func (app *proxyClient) CheckTx(ctx context.Context, req *types.RequestCheckTx) (*types.ResponseCheckTx, error) {
+	app.mtx.Lock()
+	defer app.mtx.Unlock()
 	defer addTimeSample(app.metrics.MethodTiming.With("method", "check_tx", "type", "sync"))()
 	return app.client.CheckTx(ctx, req)
 }
 
 func (app *proxyClient) Echo(ctx context.Context, msg string) (*types.ResponseEcho, error) {
+	app.mtx.Lock()
+	defer app.mtx.Unlock()
 	defer addTimeSample(app.metrics.MethodTiming.With("method", "echo", "type", "sync"))()
 	return app.client.Echo(ctx, msg)
 }
 
 func (app *proxyClient) Info(ctx context.Context, req *types.RequestInfo) (*types.ResponseInfo, error) {
+	app.mtx.Lock()
+	defer app.mtx.Unlock()
 	defer addTimeSample(app.metrics.MethodTiming.With("method", "info", "type", "sync"))()
 	return app.client.Info(ctx, req)
 }
 
 func (app *proxyClient) Query(ctx context.Context, req *types.RequestQuery) (*types.ResponseQuery, error) {
+	app.mtx.Lock()
+	defer app.mtx.Unlock()
 	defer addTimeSample(app.metrics.MethodTiming.With("method", "query", "type", "sync"))()
 	return app.client.Query(ctx, req)
 }
 
 func (app *proxyClient) ListSnapshots(ctx context.Context, req *types.RequestListSnapshots) (*types.ResponseListSnapshots, error) {
+	app.mtx.Lock()
+	defer app.mtx.Unlock()
 	defer addTimeSample(app.metrics.MethodTiming.With("method", "list_snapshots", "type", "sync"))()
 	return app.client.ListSnapshots(ctx, req)
 }
 
 func (app *proxyClient) OfferSnapshot(ctx context.Context, req *types.RequestOfferSnapshot) (*types.ResponseOfferSnapshot, error) {
+	app.mtx.Lock()
+	defer app.mtx.Unlock()
 	defer addTimeSample(app.metrics.MethodTiming.With("method", "offer_snapshot", "type", "sync"))()
 	return app.client.OfferSnapshot(ctx, req)
 }
 
 func (app *proxyClient) LoadSnapshotChunk(ctx context.Context, req *types.RequestLoadSnapshotChunk) (*types.ResponseLoadSnapshotChunk, error) {
+	app.mtx.Lock()
+	defer app.mtx.Unlock()
 	defer addTimeSample(app.metrics.MethodTiming.With("method", "load_snapshot_chunk", "type", "sync"))()
 	return app.client.LoadSnapshotChunk(ctx, req)
 }
 
 func (app *proxyClient) ApplySnapshotChunk(ctx context.Context, req *types.RequestApplySnapshotChunk) (*types.ResponseApplySnapshotChunk, error) {
+	app.mtx.Lock()
+	defer app.mtx.Unlock()
 	defer addTimeSample(app.metrics.MethodTiming.With("method", "apply_snapshot_chunk", "type", "sync"))()
 	return app.client.ApplySnapshotChunk(ctx, req)
 }
