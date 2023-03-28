e5e12caae88981fc39d87d806f0089293eaef276
Yiming Zang <yzang@twitter.com>
2023-03-28 15:28:02 -0700

    Add log

--- internal/consensus/state.go
+++ internal/consensus/state.go
@@ -2310,7 +2310,7 @@ func (cs *State) RecordMetrics(height int64, block *types.Block) {
 				block.Time.Sub(lastBlockMeta.Header.Time).Seconds(),
 			)
 		}
-		cs.logger.Info(fmt.Sprintf("[TMDEBUG] Block time for height %d is: %s at time %s, prev time %s, this time %s", height, block.Time.Sub(lastBlockMeta.Header.Time), time.Now(), lastBlockMeta.Header.Time, block.Time))
+		cs.logger.Info(fmt.Sprintf("[TMDEBUG] Block time for height %d is: %s, at time %s, prev time %s, this time %s", height, block.Time.Sub(lastBlockMeta.Header.Time), time.Now(), lastBlockMeta.Header.Time, block.Time))
 		cs.logger.Info("[TMDEBUG] ------------------------------------------------------")
 	}
 
