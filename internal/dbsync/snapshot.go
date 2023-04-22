77b8a119e9dc8234365495cc66c864f6731f7657
codchen <codchen03@gmail.com>
2023-04-22 11:39:07 +0800

    test parallel

--- internal/dbsync/snapshot.go
+++ internal/dbsync/snapshot.go
@@ -3,14 +3,17 @@ package dbsync
 import (
 	"crypto/md5"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"os"
+	"os/exec"
 	"path"
+	"sync"
 
 	"github.com/tendermint/tendermint/config"
 	dstypes "github.com/tendermint/tendermint/proto/tendermint/dbsync"
 )
+
+const WORKER_COUNT = 32
 
 func Snapshot(height uint64, dbsyncConfig config.DBSyncConfig, baseConfig config.BaseConfig) error {
 	src := path.Join(baseConfig.DBDir(), ApplicationDBSubdirectory)
@@ -32,35 +35,11 @@ func Snapshot(height uint64, dbsyncConfig config.DBSyncConfig, baseConfig config
 			wasmNames[fd.Name()] = struct{}{}
 		}
 	}
-	for _, fd := range fds {
-		var srcfp, dstfp string
-		if _, ok := wasmNames[fd.Name()]; ok {
-			srcfp = path.Join(wasmSrc, fd.Name())
-			dstfp = path.Join(dst, fd.Name()) + WasmSuffix
-		} else {
-			srcfp = path.Join(src, fd.Name())
-			dstfp = path.Join(dst, fd.Name())
-		}
 
-		var srcfd *os.File
-		var dstfd *os.File
-		if srcfd, err = os.Open(srcfp); err != nil {
-			return err
-		}
+	assignments := make([][]os.FileInfo, WORKER_COUNT)
 
-		if dstfd, err = os.Create(dstfp); err != nil {
-			srcfd.Close()
-			return err
-		}
-
-		if _, err = io.Copy(dstfd, srcfd); err != nil {
-			srcfd.Close()
-			dstfd.Close()
-			return err
-		}
-
-		srcfd.Close()
-		dstfd.Close()
+	for i, fd := range fds {
+		assignments[i%WORKER_COUNT] = append(assignments[i%WORKER_COUNT], fd)
 	}
 
 	metadata := dstypes.MetadataResponse{
@@ -68,21 +47,68 @@ func Snapshot(height uint64, dbsyncConfig config.DBSyncConfig, baseConfig config
 		Filenames:   []string{},
 		Md5Checksum: [][]byte{},
 	}
+	metadataMtx := &sync.Mutex{}
 
-	for _, fd := range fds {
-		filename := fd.Name()
-		if _, ok := wasmNames[fd.Name()]; ok {
-			filename += WasmSuffix
-		}
-		metadata.Filenames = append(metadata.Filenames, filename)
+	wg := sync.WaitGroup{}
+	for i := 0; i < WORKER_COUNT; i++ {
+		wg.Add(1)
+		assignment := assignments[i]
+		go func() {
+			for _, fd := range assignment {
+				var srcfp, dstfp string
+				if _, ok := wasmNames[fd.Name()]; ok {
+					srcfp = path.Join(wasmSrc, fd.Name())
+					dstfp = path.Join(dst, fd.Name()) + WasmSuffix
+				} else {
+					srcfp = path.Join(src, fd.Name())
+					dstfp = path.Join(dst, fd.Name())
+				}
 
-		bz, err := ioutil.ReadFile(path.Join(dst, filename))
-		if err != nil {
-			return err
-		}
-		sum := md5.Sum(bz)
-		metadata.Md5Checksum = append(metadata.Md5Checksum, sum[:])
+				// var srcfd *os.File
+				// var dstfd *os.File
+				// if srcfd, err = os.Open(srcfp); err != nil {
+				// 	panic(err)
+				// }
+
+				// if dstfd, err = os.Create(dstfp); err != nil {
+				// 	srcfd.Close()
+				// 	panic(err)
+				// }
+
+				// if _, err = io.Copy(dstfd, srcfd); err != nil {
+				// 	srcfd.Close()
+				// 	dstfd.Close()
+				// 	panic(err)
+				// }
+				cmd := exec.Command("cp", srcfp, dstfp)
+				_, err := cmd.Output()
+				if err != nil {
+					panic(err)
+				}
+
+				filename := fd.Name()
+				if _, ok := wasmNames[fd.Name()]; ok {
+					filename += WasmSuffix
+				}
+
+				bz, err := ioutil.ReadFile(path.Join(dst, filename))
+				if err != nil {
+					panic(err)
+				}
+				sum := md5.Sum(bz)
+
+				metadataMtx.Lock()
+				metadata.Filenames = append(metadata.Filenames, filename)
+				metadata.Md5Checksum = append(metadata.Md5Checksum, sum[:])
+
+				metadataMtx.Unlock()
+				// srcfd.Close()
+				// dstfd.Close()
+			}
+			wg.Done()
+		}()
 	}
+	wg.Wait()
 
 	metadataBz, err := metadata.Marshal()
 	if err != nil {
