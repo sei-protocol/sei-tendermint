936aa27d850f03d8fec4a3d9383449923531e182
yzang2019 <zymfrank@gmail.com>
2024-02-06 13:57:08 +0800

    Add log

--- internal/rpc/core/mempool.go
+++ internal/rpc/core/mempool.go
@@ -45,6 +45,10 @@ func (env *Environment) BroadcastTx(ctx context.Context, req *coretypes.RequestB
 			select {
 			case <-ctx.Done():
 			case resCh <- res:
+				if res.Code != 0 {
+					fmt.Printf("[TM-DEBUG] Got CheckTx response: %v \n", res)
+				}
+
 			}
 		},
 		mempool.TxInfo{},
