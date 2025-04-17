64a9d75882ead23078bb406cbab65e704b9a8244
yzang2019 <zymfrank@gmail.com>
2025-04-17 15:30:13 +0800

    Fix setBlock true false

--- internal/blocksync/pool.go
+++ internal/blocksync/pool.go
@@ -664,7 +664,10 @@ func (*bpRequester) OnStop() {}
 // Returns true if the peer matches and block doesn't already exist.
 func (bpr *bpRequester) setBlock(block *types.Block, extCommit *types.ExtendedCommit, peerID types.NodeID) bool {
 	bpr.mtx.Lock()
-	if bpr.block != nil || bpr.peerID != peerID {
+	if bpr.block != nil {
+		bpr.mtx.Unlock()
+		return true
+	} else if bpr.peerID != peerID {
 		bpr.mtx.Unlock()
 		return false
 	}
