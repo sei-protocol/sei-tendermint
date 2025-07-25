0fd57fa49e3826bee12102d96a96cfda856d949d
blindchaser <zengyiren0@gmail.com>
2025-07-25 16:45:15 -0400

    fmt

--- internal/consensus/state.go
+++ internal/consensus/state.go
@@ -42,9 +42,9 @@ import (
 var (
 	ErrInvalidProposalSignature     = errors.New("error invalid proposal signature")
 	ErrInvalidProposalPOLRound      = errors.New("error invalid proposal POL round")
-	ErrInvalidProposalPartSetHeader = errors.New("error invalid proposal part set header")
 	ErrAddingVote                   = errors.New("error adding vote")
 	ErrSignatureFoundInPastBlocks   = errors.New("found signature from the same key")
+	ErrInvalidProposalPartSetHeader = errors.New("error invalid proposal part set header")
 
 	errPubKeyIsNotSet = errors.New("pubkey is not set. Look for \"Can't get private validator pubkey\" errors")
 )
