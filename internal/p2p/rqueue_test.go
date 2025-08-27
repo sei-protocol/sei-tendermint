1c76fb69bddf4ce20e48c6916ac72ff3611ce9f9
Grzegorz Prusak <pompon.pompon@gmail.com>
2025-08-27 10:49:51 +0200

    queue tests

--- internal/p2p/rqueue_test.go
+++ internal/p2p/rqueue_test.go
@@ -1,23 +1,80 @@
 package p2p
 
 import (
+	"context"
+	"github.com/tendermint/tendermint/libs/utils"
+	"github.com/tendermint/tendermint/libs/utils/scope"
+	"slices"
 	"testing"
 )
 
-func TestSimpleQueue(t *testing.T) {
+func TestQueuePruning(t *testing.T) {
 	ctx := t.Context()
-
-	// set up a small queue with very small buffers so we can
-	// watch it shed load, then send a bunch of messages to the
-	// queue, most of which we'll watch it drop.
-	sq := NewQueue(1)
+	rng := utils.TestRng()
+	n := 20
+	var want []int
+	sq := NewQueue(n)
 	for range 100 {
-		sq.Send(Envelope{From: "merlin"}, 0)
+		// Send a bunch of messages.
+		for range 30 {
+			// priority is not part of the envelope currently,
+			// so we hack it by encoding it as a ChannelID.
+			v := ChannelID(rng.Int())
+			sq.Send(Envelope{From: "merlin", ChannelID: v}, int(v))
+			want = append(want, int(v))
+		}
+
+		// Low priority messages should be dropped.
+		slices.Sort(want)
+		l := len(want)
+		want = want[l-n:]
+		if len(want) != sq.Len() {
+			t.Fatalf("expected len %d, got %d", len(want), sq.Len())
+		}
+
+		// Receive a bunch of messages.
+		for range 5 {
+			got, err := sq.Recv(ctx)
+			if err != nil {
+				t.Fatal(err)
+			}
+			l := len(want)
+			if got, want := int(got.ChannelID), want[l-1]; got != want {
+				t.Fatalf("sq.Recv() = %d, want %d", got, want)
+			}
+			want = want[:l-1]
+		}
+		if len(want) != sq.Len() {
+			t.Fatalf("expected len %d, got %d", len(want), sq.Len())
+		}
 	}
-	if _, err := sq.Recv(ctx); err != nil {
+}
+
+// Test that receivers are notified when a message is available.
+func TestQueueConcurrency(t *testing.T) {
+	ctx := t.Context()
+	q1, q2 := NewQueue(1), NewQueue(1)
+
+	if err := utils.IgnoreCancel(scope.Run(ctx, func(ctx context.Context, s scope.Scope) error {
+		s.SpawnBg(func() error {
+			// Echo task.
+			for {
+				msg, err := q1.Recv(ctx)
+				if err != nil {
+					return err
+				}
+				q2.Send(msg, 0)
+			}
+		})
+		// Send and receive a bunch of messages.
+		for range 100 {
+			q1.Send(Envelope{From: "merlin"}, 0)
+			if _, err := q2.Recv(ctx); err != nil {
+				return err
+			}
+		}
+		return nil
+	})); err != nil {
 		t.Fatal(err)
-	}
-	if sq.Len() != 0 {
-		t.Fatalf("queue length is %d, should be 0", sq.Len())
 	}
 }
