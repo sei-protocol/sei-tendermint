20f2ed49792537c68fc4d1dbbd88113a545ca5b2
Grzegorz Prusak <pompon.pompon@gmail.com>
2025-09-09 14:50:41 +0200

    max 100txs/block

--- internal/mempool/mempool.go
+++ internal/mempool/mempool.go
@@ -512,7 +512,7 @@ func (txmp *TxMempool) ReapMaxBytesMaxGas(maxBytes, maxGasWanted, maxGasEstimate
 	txmp.priorityIndex.ForEachTx(func(wtx *WrappedTx) bool {
 		size := types.ComputeProtoSizeForTxs([]types.Tx{wtx.tx})
 
-		if maxBytes > -1 && totalSize+size > maxBytes || len(txs)>=60 {
+		if maxBytes > -1 && totalSize+size > maxBytes || len(txs)>=100 {
 			return false
 		}
 
