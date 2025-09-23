a34b4d188097be3e97edab474c28726af4c72de0
Grzegorz Prusak <pompon.pompon@gmail.com>
2025-09-23 20:42:22 +0200

    docs

--- libs/utils/scope/global.go
+++ libs/utils/scope/global.go
@@ -4,6 +4,7 @@ import (
 	"context"
 )
 
+// GlobalHandle is a handle to a task spawned via SpawnGlobal.
 type GlobalHandle struct {
 	ctx context.Context
 	cancel context.CancelFunc
@@ -11,6 +12,9 @@ type GlobalHandle struct {
 	err error
 }
 
+// SpawnGlobal spawns a task in a global context.
+// Use with care, as it is not tied to any scope and must be closed manually by calling Close().
+// Can be used as an intermediate step when migrating code to use scopes.
 func SpawnGlobal(task func(ctx context.Context) error) *GlobalHandle {
 	ctx, cancel := context.WithCancel(context.Background())
 	h := &GlobalHandle{
@@ -25,10 +29,13 @@ func SpawnGlobal(task func(ctx context.Context) error) *GlobalHandle {
 	return h
 }
 
+// Done returns a channel that is closed when the task is finished.
 func (h *GlobalHandle) Done() <-chan struct{} {
 	return h.done
 }
 
+// Err returns the task's result if it finished, or nil if it is still running.
+// Note that if task succeeded, nil is returned.
 func (h *GlobalHandle) Err() error {
 	select {
 	case <-h.done:
@@ -38,6 +45,8 @@ func (h *GlobalHandle) Err() error {
 	}
 }
 
+// Close cancels the task and waits for it to finish.
+// Returns the task's result.
 func (h *GlobalHandle) Close() error {
 	h.cancel()
 	<-h.done
